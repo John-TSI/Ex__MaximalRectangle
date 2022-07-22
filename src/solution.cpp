4b4620066f1c1c31ad9261264ea42ba271a9d909
John Roughley <jroughley@softwareinstitute.com>
2022-07-22 13:31:42 +0100

    fixed two bugs, commented code

--- src/solution.cpp
+++ src/solution.cpp
@@ -7,7 +7,7 @@
 int Solution::maximalRectangle(std::vector<std::vector<char>>& matrix)
 {
     int rows = matrix.size(), cols = matrix[0].size(), area = rows*cols;
-    if(area == 1){ return (int) (matrix[0][0] == '1'); }
+    if(area == 1){ return (int) (matrix[0][0] == '1'); }  // handle single element case
 
     // check whether matrix is all ones / all zeros
     bool allOnes{true}, allZeros{false};
@@ -52,27 +52,39 @@ int Solution::maximalRectangle(std::vector<std::vector<char>>& matrix)
 int Solution::maximalRectangle2(std::vector<std::vector<char>>& matrix)
 {
     int rows = matrix.size(), cols = matrix[0].size(), maxArea{0};
-    if(rows*cols == 1){ return (int) (matrix[0][0] == '1'); }
-    std::vector<int> rowData(cols,0);
+    if(rows*cols == 1){ return (int) (matrix[0][0] == '1'); }  // handle single element case
+
+    std::vector<int> rowData(cols,0);  // stores cumulative sums of matrix cols, acts as a histogram of # of consecutive '1's as function of column index.
+                                    // from here, a column's 'bar' refers to its cumulative sum --> higher bar == greater total sum   
 
     for(int row{0}; row<rows; ++row)
     {
-        std::stack<int> idxStack{{-1}};
+        std::stack<int> idxStack{{-1}};  // stores indices of columns with bar at least as high as previous column (its 'neighbour')
         for(int col{0}; col<cols; ++col)
         { 
-            rowData[col] += (matrix[row][col] - '0');
+            // bars are updated per row
+            if(matrix[row][col] == '0'){ rowData[col] = 0; }  // reset column total if interrupted by a '0'
+            else{ rowData[col] += (matrix[row][col] - '0'); }
             
-            if(idxStack.top() == -1 || rowData[col] >= rowData[idxStack.top()]){ idxStack.push(col); }
-            else
+            if(idxStack.top() == -1 || rowData[col] >= rowData[idxStack.top()]){ idxStack.push(col); }   // if first col to be added to stack, or col has bar not lower than col at top of stack
+            else  // col has lower bar than neighbour, preventing extension of rectangle of neighbour's bar height
             {
+                // step back through the stack to determine the maximum area rectangle of height equal to neighbour's bar
                 while(idxStack.top() != -1 && rowData[idxStack.top()] >= rowData[col])
                 {
-                    int currIdx = idxStack.top(); idxStack.pop(); int leftIdx = idxStack.top(); 
-                    int area = rowData[currIdx]*(col - leftIdx - 1);
+                    int nbrIdx = idxStack.top(); idxStack.pop(); int leftIdx = idxStack.top();  // nbrIdx is index of neighbour
+                    int area = rowData[nbrIdx]*(col - leftIdx - 1);  // width of rectangle is the index distance between previous stack bar (leftIdx) and the current lower bar (col), minus 1
                     if(area > maxArea){ maxArea = area; }
                 }
-                idxStack.push(col);
+                idxStack.push(col);  // after all the stack pops, current bar becomes the next highest stack bar
             } 
+        }
+        // reached end of row, but also need to consider possible rectangles using bars still on the stack
+        while(idxStack.top() != -1)
+        {
+            int rightIdx{cols}, nbrIdx = idxStack.top(); idxStack.pop(); int leftIdx = idxStack.top();  // only difference compared to above: rectangles extend to include last column
+            int area = rowData[nbrIdx]*(rightIdx - leftIdx - 1);
+            if(area > maxArea){ maxArea = area; }
         }
     }
     return maxArea;
